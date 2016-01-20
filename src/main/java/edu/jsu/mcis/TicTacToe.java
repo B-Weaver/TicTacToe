bcd0654b6c933ba30eae77d9310248ce237cfa41
Barry Weaver <Barry Weaver>
2016-01-20 03:55:38 -0600

    Created the model and wrote main

--- src/main/java/edu/jsu/mcis/TicTacToe.java
+++ src/main/java/edu/jsu/mcis/TicTacToe.java
@@ -1,12 +1,52 @@
-package edu.jsu.mcis;
+//package edu.jsu.mcis;
+
+import java.util.*;
 
 public class TicTacToe {
-
-
-
-
-
-	public static void main(String[] args) {
+		private TicTacToeModel game;
+		private int turn;
+		private int row;
+		private int col;
+		private String[] player;
+		
+		public TicTacToe(){
+			game = new TicTacToeModel();
+			player = new String[2];
+			player[0] = "Player X";
+			player[1] = "Player O";
+			turn = 0;
+			row = 0;
+			col = 0;
+		}
+		
+		public void runGame(){
+			System.out.println("Welcome to TicTacToe.");
+			
+			Scanner choice = new Scanner(System.in);
+			
+			while(game.resultString().equals("NONE")){
+				if(game.xTurn) turn = 0;
+				else turn = 1;
+				
+				System.out.println(player[turn] + " choose a row 1-3.");
+				row = choice.nextInt();
+				System.out.println(player[turn] + " choose a column 1-3.");
+				col = choice.nextInt();
+				
+				game.setMark(row-1, col-1);
+				
+				System.out.print("\n\n" + game.toString() + "\n\n");
+				
+				if(!game.resultString().equals("NONE"))
+					System.out.println(game.resultString());
+				
+			}
+		}
+			
+			
+public static void main(String[] args) {
+		TicTacToe newGame = new TicTacToe();
+		newGame.runGame();
 		
 	}
 }
--- src/main/java/edu/jsu/mcis/TicTacToeModel.java
+++ src/main/java/edu/jsu/mcis/TicTacToeModel.java
@@ -1,8 +1,133 @@
-package edu.jsu.mcis;
+//package edu.jsu.mcis;
 
 public class TicTacToeModel{
+	public enum Mark {EMPTY, X, O};
+    public enum Result {NONE, XWINS, OWINS, TIE};
+    private Mark[][] grid;
+    protected boolean xTurn;
+	
 	
 	public TicTacToeModel(){
-		
+		grid = new Mark[3][3];
+        reset();
 	}
+	
+	private void reset() {
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                grid[i][j] = Mark.EMPTY;
+            }
+        }
+        xTurn = true;
+    }
+    
+    public void setMark(int r, int c) {
+        if(r >= 0 && r < 3 && c >= 0 && c < 3) {
+            if(grid[r][c] == Mark.EMPTY) {
+                if(xTurn) grid[r][c] = Mark.X;
+                else grid[r][c] = Mark.O;
+                xTurn = !xTurn;
+            }
+        }
+        else {
+			System.out.println("Invalid input");
+		}
+		
+		
+    }
+	
+	public String resultString(){
+		Result result = getResult();
+		String r = "";
+		if(result == Result.XWINS)
+			r = "Player X Wins!";
+		else if(result == Result.OWINS)
+			r = "Player O WINS!";
+		else if(result == Result.TIE)
+			r = "It's a Tie!";
+		else
+			r = "NONE";
+		
+		return r;
+	}
+    
+    public Result getResult() {
+        Result r = checkResultHorizontally();
+        if(r != Result.NONE) return r;
+        r = checkResultVertically();
+        if(r != Result.NONE) return r;
+        r = checkResultDiagonally();
+        if(r != Result.NONE) return r;
+        if(isBoardFull()) return Result.TIE;        
+        return Result.NONE;
+    }
+    
+    private Result checkResultHorizontally() {
+        for(int i = 0; i < 3; i++) {
+            if(grid[i][0] == grid[i][1] && 
+               grid[i][1] == grid[i][2] &&
+               grid[i][0] != Mark.EMPTY) {
+                if(grid[i][0] == Mark.X) return Result.XWINS;
+                else return Result.OWINS;
+            }
+        }
+        return Result.NONE;
+    }
+    
+    private Result checkResultVertically() {
+        for(int i = 0; i < 3; i++) {
+            if(grid[0][i] == grid[1][i] && 
+               grid[1][i] == grid[2][i] &&
+               grid[0][i] != Mark.EMPTY) {
+                if(grid[0][i] == Mark.X) return Result.XWINS;
+                else return Result.OWINS;
+            }
+        }
+        return Result.NONE;
+    }
+    
+    private Result checkResultDiagonally() {
+        if(grid[0][0] == grid[1][1] &&
+           grid[1][1] == grid[2][2] &&
+           grid[0][0] != Mark.EMPTY) {
+            if(grid[0][0] == Mark.X) return Result.XWINS;
+            else return Result.OWINS;
+        }
+        if(grid[2][0] == grid[1][1] &&
+           grid[1][1] == grid[0][2] &&
+           grid[2][0] != Mark.EMPTY) {
+            if(grid[2][0] == Mark.X) return Result.XWINS;
+            else return Result.OWINS;
+        }
+        return Result.NONE;
+    }
+    
+    protected boolean isBoardFull() {
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(grid[i][j] == Mark.EMPTY) return false;
+            }
+        }
+        return true;
+    }
+    
+    public String toString() {
+        String s = "";
+        for(int i = 0; i < 3; i++) {
+			if(i == 1 || i == 2) s += "---------\n";
+            for(int j = 0; j < 3; j++) {
+                if(grid[i][j] == Mark.X) s += "X";
+                else if(grid[i][j] == Mark.O) s += "O";
+                else s += " ";
+				if(j == 0 || j == 1) s += " | ";
+				if(j == 2) s += "\n";
+            }
+        }
+        return s;
+    }
+    
+    public int getTurn() {
+        if(xTurn) return 0;
+        else return 1;
+    }
 }
